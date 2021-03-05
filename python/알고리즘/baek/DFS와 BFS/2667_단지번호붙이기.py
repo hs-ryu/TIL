aad1a56d0d75feb339636842ea5981cbfe62e510
ryu <munam4@naver.com>
2021-03-06 01:11:23 +0900

    백준 예제 추가

--- python/알고리즘/baek/DFS와 BFS/2667_단지번호붙이기.py
+++ python/알고리즘/baek/DFS와 BFS/2667_단지번호붙이기.py
@@ -12,9 +12,37 @@
 출력
 첫 번째 줄에는 총 단지수를 출력하시오. 그리고 각 단지내 집의 수를 오름차순으로 정렬하여 한 줄에 하나씩 출력하시오.
 '''
-#3/5 저녁에 풀기
+
+dr = [-1, 1, 0 ,0]
+dc = [0, 0, -1, 1]
+
+def BFS(sr, sc):
+    global cnt
+    visited[sr][sc] = 1
+    Q = [[sr,sc]]
+    total = 1
+    while Q:
+        cr, cc = Q.pop(0)
+        for i in range(4):
+            r = cr + dr[i]
+            c = cc + dc[i]
+            if 0 <= r < N and 0 <= c < N and jido[r][c] and visited[r][c] == 0:
+                visited[r][c] = 1
+                total += 1
+                Q.append([r,c])
+    return total
 
 N = int(input())
 
 jido = [list(map(int,input())) for _ in range(N)]
-print(jido)
+visited = [[0 for _ in range(N)] for _ in range(N)]
+result = []
+for i in range(N):
+    for j in range(N):
+        if jido[i][j] and visited[i][j] == 0:
+            sr, sc = i,j
+            result.append(BFS(sr, sc))
+result.sort()
+print(len(result))
+for i in result:
+    print(i)
