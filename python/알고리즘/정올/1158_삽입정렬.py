5515d65cbd282108e1756f53277b1e8ae4226714
ryu <munam4@naver.com>
2021-07-08 18:22:50 +0900

    0708 정올 1문제 추가

---
+++ python/알고리즘/정올/1158_삽입정렬.py
@@ -0,0 +1,9 @@
+n = int(input())
+nums = list(map(int,input().split()))
+
+for i in range(1,n):
+    for j in range(i,0,-1):
+        if nums[j-1] > nums[j]:
+            nums[j-1], nums[j] = nums[j], nums[j-1]
+    print(i)
+    print(*nums)
