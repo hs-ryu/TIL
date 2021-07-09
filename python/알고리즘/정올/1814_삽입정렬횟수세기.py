300a1325ce73db800fe765b6909cc6656ac07ddc
ryu <munam4@naver.com>
2021-07-09 17:39:19 +0900

    0709 정올 2문제

--- python/알고리즘/정올/1814_삽입정렬횟수세기.py
+++ python/알고리즘/정올/1814_삽입정렬횟수세기.py
@@ -1,8 +1,9 @@
 n = int(input())
 nums = list(map(int,input().split()))
-
+cnt = 0
 for i in range(1,n):
     for j in range(i,0,-1):
         if nums[j-1] > nums[j]:
             nums[j-1], nums[j] = nums[j], nums[j-1]
-    print(*nums)
+            cnt += 1
+print(cnt)
