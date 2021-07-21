280ab56ae6f67e570fa15d40c40447102133a059
ryu <munam4@naver.com>
2021-07-21 23:16:01 +0900

    0721 정올 1문제 추가

---
+++ python/알고리즘/정올/2567_싸이클.py
@@ -0,0 +1,11 @@
+
+def calc(num,arr):
+    s = num * n % p
+    if s in arr:
+        print(len(arr)-arr.index(s))
+        return
+    arr.append(s)
+    calc(s,arr)
+
+n, p = map(int,input().split())
+calc(n,[n])
