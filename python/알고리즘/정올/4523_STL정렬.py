711e6577309f7bdb95489156ee4e757ea5215268
ryu <munam4@naver.com>
2021-08-20 01:31:08 +0900

    0820 정올 1문제

---
+++ python/알고리즘/정올/4523_STL정렬.py
@@ -0,0 +1,18 @@
+n = int(input())
+arr = list(map(int,input().split()))
+s,e = map(int,input().split())
+arr2 = arr[s:e+1]
+range_arr2 = sorted(arr2)
+if s == 0:
+    if e == n-1:
+        result = range_arr2
+    else:
+        result = arr[:e] + range_arr2
+else:
+    if e == n-1:
+        result = arr[:s] + range_arr2
+    else:
+        result = arr[:s] + range_arr2 + arr[e+1:]
+print(*result)
+print(*sorted(result))
+    
