19f2cf47ed711ab51341caf33fb3ce7f71b7c0af
ryu <munam4@naver.com>
2021-10-02 00:24:09 +0900

    1002 프로그래머스 1문제

---
+++ python/알고리즘/pro/lev1_핸드폰번호가리기.js
@@ -0,0 +1,10 @@
+function solution(phone_number) {
+  var answer = '';
+  
+  for (var i = 0; i < phone_number.length - 4; i++ ) {
+      answer += '*'
+  }
+  answer += phone_number.slice(-4)
+  
+  return answer;
+}
