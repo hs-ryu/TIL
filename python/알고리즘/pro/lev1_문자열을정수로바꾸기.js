22756634c74f0c1aac57b73dbd20847cb70e93ec
ryu <munam4@naver.com>
2021-10-08 02:33:31 +0900

    1008 프로그래머스 1문제(js)

---
+++ python/알고리즘/pro/lev1_문자열을정수로바꾸기.js
@@ -0,0 +1,13 @@
+function solution(s) {
+  var answer = 0;
+  if (s[0] === '+') {
+      answer = Number(s.slice(1))
+  }
+  else if(s[0] === '-') {
+      answer = Number(s.slice(1)) * (-1)
+  }
+  else {
+      answer = Number(s)
+  }
+  return answer;
+}
