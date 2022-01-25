75972fa6ebf6558402941ca44c9c70b392aef469
ryu <munam4@naver.com>
2022-01-26 02:25:09 +0900

    220126 백준 1문제 - 2745

---
+++ python/알고리즘/baek/수학/2745_진법변환.py
@@ -0,0 +1,23 @@
+
+# n진수 -> 10진수
+s,n = input().split()
+n = int(n)
+print(int(s,n))
+
+# 10진수 -> 2, 8, 16 진수
+# bin(10)
+# oct(10)
+# hex(10)
+
+# 10진수 -> n진수
+# k : 10진수 수, n -> n진수 수
+
+# answer = []
+# while k > 0:
+#     k = k // n
+#     mod = k % n
+#     answer.append(mod)
+# print(''.join(answer.reverse()))
+
+# n진수 -> n진수
+# n진수를 10진수로 변호나 뒤 다시 n진수 변환
