991cc7a572c697f9170f2e5850888f14dc14d0a0
ryu <munam4@naver.com>
2022-05-20 12:24:14 +0900

    220520 프로그래머스 1문제 - 피보나치수

---
+++ python/알고리즘/pro/lev2_피보나치수.py
@@ -0,0 +1,10 @@
+
+# dp로 풀기. 재귀로 하면 시간초과남.
+def solution(n):
+    dp = [0] * (n + 1)
+    dp[0] = 0
+    dp[1] = 1
+    for i in range(2,n+1):
+        dp[i] = dp[i-2] + dp[i-1]
+    answer = dp[n] % 1234567
+    return answer
