4f0bfda54c034785c7f3ede2e3fb8ea7b6cdff9a
ryu <munam4@naver.com>
2022-05-23 12:20:57 +0900

    220523 프로그래머스 1문제 - 가장큰정사각형찾기(failed)

--- python/알고리즘/pro/lev2_피보나치수.py
+++ python/알고리즘/pro/lev2_피보나치수.py
@@ -1,5 +1,9 @@
 
 # dp로 풀기. 재귀로 하면 시간초과남.
+
+import time
+start = time.time()
+
 def solution(n):
     dp = [0] * (n + 1)
     dp[0] = 0
@@ -8,3 +12,16 @@ def solution(n):
         dp[i] = dp[i-2] + dp[i-1]
     answer = dp[n] % 1234567
     return answer
+print(solution(10000))
+print("time :", time.time() - start)
+
+
+start = time.time()
+def solution1(n):
+    dp = [0,1]
+    for i in range(2,n+1):
+        dp.append(dp[i-1] + dp[i-2])
+    answer = dp[n] % 1234567
+    return answer
+print(solution1(10000))
+print("time :", time.time() - start)
