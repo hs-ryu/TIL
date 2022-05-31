a4fe3950f9fff85301143ce22a7addc7db8593ff
ryu <munam4@naver.com>
2022-05-31 11:43:46 +0900

    220531 프로그래머스 1문제 - 점프와순간이동(failed)

---
+++ python/알고리즘/pro/lev2_점프와순간이동.py
@@ -0,0 +1,14 @@
+# BFS? DP?
+def solution(n):
+    ans = 0
+    dp = [0] * (n+1)
+    dp[0] = 0
+    dp[1] = 1
+    for i in range(2,n+1):
+        if i % 2 == 0:
+            dp[i] = dp[i//2]
+        else:
+            dp[i] = dp[i-1] + 1
+            
+    ans = dp[n]
+    return ans
