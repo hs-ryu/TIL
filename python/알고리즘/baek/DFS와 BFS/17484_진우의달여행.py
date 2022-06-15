1a4fd60cf77027dc2817775179d0a57c79849f5d
ryu <munam4@naver.com>
2022-06-15 13:26:50 +0900

    220615 백준 1문제 - 17484_진우의달여행

--- python/알고리즘/baek/DFS와 BFS/17484_진우의달여행.py
+++ python/알고리즘/baek/DFS와 BFS/17484_진우의달여행.py
@@ -7,14 +7,16 @@ dc = [-1,0,1]
 n, m = map(int,input().split())
 space = [list(map(int,input().split())) for _ in range(n)]
 
+# 같은 방향으로 두번 갈 수 없다는거 체크해야함.
 result = 0
 for r in range(1,n):
     for c in range(m):
+        sum_list = []
         for k in range(3):
             br = r + dr[k]
             bc = c + dc[k]
-            if 0 <= br and 0 <= bc:
-                print("??")
-
-
+            if 0 <= bc < m:
+                sum_list.append(space[r][c] + space[br][bc])
+        space[r][c] = min(sum_list)
 print(space)
+
