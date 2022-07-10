e456591d87bad9b6b2dc883499ad83ae466cd53e
ryu <munam4@naver.com>
2022-07-10 15:36:33 +0900

    220710 백준 1문제 - 19948_음유시인영재

--- python/알고리즘/baek/구현/19948_음유시인영재.py
+++ python/알고리즘/baek/구현/19948_음유시인영재.py
@@ -4,10 +4,14 @@ poem = input()
 spacebar = int(input())
 possible_alpha = list(map(int,input().split()))
 
+# 입력받은 알파벳 제한을 넣어줄 딕셔너리
 alpha = dict()
 for i in range(26):
     alpha[chr(65+i)] = possible_alpha[i]
+# 스페이스바도 넣어주고
 alpha[" "] = spacebar
+
+# 문자열 돌면서 카운트 측정
 count_alpha = dict()
 count_alpha[poem[0].upper()] = 1
 for i in range(1,len(poem)):
@@ -17,11 +21,14 @@ for i in range(1,len(poem)):
         count_alpha[poem[i].upper()] += 1
     else:
         count_alpha[poem[i].upper()] = 1
+
+# 측정한 개수와, 제한 사항 비교
 for k in count_alpha.keys():
     v = count_alpha[k]
     if v > alpha[k]:
         print(-1)
         break
+# 브레이크 안걸리면, 문제 없는거임.
 else:
     poem = poem.split()
     result = ""
