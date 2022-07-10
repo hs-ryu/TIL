7ff290d8782580eaa1a11af0f3b10db1f90b70ce
ryu <munam4@naver.com>
2022-07-10 15:34:45 +0900

    220710 백준 1문제 - 19948_음유시인영재

--- python/알고리즘/baek/구현/19948_음유시인영재.py
+++ python/알고리즘/baek/구현/19948_음유시인영재.py
@@ -1,3 +1,5 @@
+
+# 100% 에서 틀림...
 poem = input()
 spacebar = int(input())
 possible_alpha = list(map(int,input().split()))
@@ -5,29 +7,24 @@ possible_alpha = list(map(int,input().split()))
 alpha = dict()
 for i in range(26):
     alpha[chr(65+i)] = possible_alpha[i]
-
+alpha[" "] = spacebar
 count_alpha = dict()
-for i in range(len(poem)):
-    k = poem[i].upper()
-    if k in count_alpha:
-        count_alpha[k] += 1
+count_alpha[poem[0].upper()] = 1
+for i in range(1,len(poem)):
+    if poem[i] == poem[i-1]:
+        continue
+    if poem[i].upper() in count_alpha:
+        count_alpha[poem[i].upper()] += 1
     else:
-        count_alpha[k] = 1
-
-
-
-for alpha in count_alpha.keys():
-    if alpha == " ":
-        if count_alpha[alpha] > spacebar:
-            print(-1)
-            break
-    else:
-        if count_alpha[alpha] > possible_alpha[alpha]:
-            print(-1)
-            break
+        count_alpha[poem[i].upper()] = 1
+for k in count_alpha.keys():
+    v = count_alpha[k]
+    if v > alpha[k]:
+        print(-1)
+        break
 else:
     poem = poem.split()
     result = ""
     for i in range(len(poem)):
-        result += poem[i][0]
+        result += poem[i][0].upper()
     print(result)
