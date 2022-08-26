51cfaa1887c3e6d3bc8771a4cf92e060c9862b7b
ryu <munam4@naver.com>
2022-08-26 14:01:17 +0900

    220826 백준 1문제 - 15686_치킨배달(failed)

--- python/알고리즘/baek/구현/15686_치킨배달.py
+++ python/알고리즘/baek/구현/15686_치킨배달.py
@@ -1,5 +1,29 @@
+def search(level, arr, k):
+    if k == len(chickens):
+        return
+    if level == m:
+        # 여기서 길이 체크하기.
+        return
+    
+    for i in range(k, len(chickens)):
+        pass
+
+
 n,m = map(int,input().split())
 
 city = []
 for i in range(n):
     city.append(list(map(int,input().split())))
+
+# 1. 치킨집의 위치를 다 찾는다. 집의 위치도 미리 다 찾아 놓는다.
+# 2. 치킨집 중에서 m개를 고른다. (조합)
+# 3. 집에서 치킨집의 위치를 정한다.
+
+homes = []
+chickens = []
+for i in range(n):
+    for j in range(n):
+        if city[i][j] == 1:
+            homes.append([i,j])
+        elif city[i][j] == 2:
+            chickens.append([i,j])
