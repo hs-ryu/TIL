bd64e750a3d018e5227fa02ccfdfbbce154b0f51
ryu <munam4@naver.com>
2022-08-27 13:33:41 +0900

    220827 백준 1문제 - 15686_치킨배달

--- python/알고리즘/baek/구현/15686_치킨배달.py
+++ python/알고리즘/baek/구현/15686_치킨배달.py
@@ -1,12 +1,27 @@
-def search(level, arr, k):
-    if k == len(chickens):
+def search(level, arr):
+    if level == len(chickens):
+        if len(arr) == m:
+            # 여기서 체크하면 될듯. 2중 반복문 써서, 가까운 애들 쳌 해버리자.
+            # print(arr)
+            global result
+            temp_result = 0
+            for i in range(len(homes)):
+                # 집 하나하나에서 부터 가까운 치킨집 거리 구하기.
+                temp_home = homes[i]
+                temp_total_length = 101
+                for j in range(len(arr)):
+                    temp_chicken = arr[j]
+                    # print("현재 체크 집, 치킨집 : ", temp_home, temp_chicken)
+                    temp_length = abs(temp_home[0] - temp_chicken[0]) + abs(temp_home[1] - temp_chicken[1])
+                    temp_total_length = min(temp_total_length,temp_length)
+                temp_result += temp_total_length
+            result = min(temp_result, result)
         return
-    if level == m:
-        # 여기서 길이 체크하기.
-        return
-    
-    for i in range(k, len(chickens)):
-        pass
+    arr.append(chickens[level])
+    search(level+1,arr)
+    arr.pop(-1)
+    search(level+1,arr)
+
 
 
 n,m = map(int,input().split())
@@ -21,9 +36,13 @@ for i in range(n):
 
 homes = []
 chickens = []
+result = 101 * 14
 for i in range(n):
     for j in range(n):
         if city[i][j] == 1:
             homes.append([i,j])
         elif city[i][j] == 2:
             chickens.append([i,j])
+# print(chickens)
+search(0,[])
+print(result)
