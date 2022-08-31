b3b38f219c84187e131ae639c5c930777ba98883
ryu <munam4@naver.com>
2022-08-31 23:51:44 +0900

    220831 백준 1문제 - 15683_감시(failed)

--- python/알고리즘/baek/구현/15683_감시.py
+++ python/알고리즘/baek/구현/15683_감시.py
@@ -1,3 +1,6 @@
+def search():
+    pass
+
 n, m = map(int,input().split())
 
 
@@ -5,9 +8,10 @@ office = []
 for _ in range(n):
     office.append(list(map(int,input().split())))
 
-
 cctvs = []
 for i in range(n):
     for j in range(m):
         if office[i][j] != 0 and office[i][j] != 6:
             cctvs.append([i,j,office[i][j]])
+
+
