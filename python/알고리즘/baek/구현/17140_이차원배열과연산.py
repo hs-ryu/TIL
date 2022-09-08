dbabaebf8ef7b443efe785d703ff933e0a3e8b51
ryu <munam4@naver.com>
2022-09-08 19:34:20 +0900

    220908 백준 1문제 - 17140_이차원배열과연산(failed)

---
+++ python/알고리즘/baek/구현/17140_이차원배열과연산.py
@@ -0,0 +1,24 @@
+
+
+
+r,c,k = map(int,input().split())
+a = [list(map(int,input().split())) for _ in range(3)]
+
+# 0,0을 기준으로 맞춰주기
+r -= 1
+c -= 1
+
+while True:
+    if a[r][c] == k:
+        break
+    # 행이 열보다 큰 경우
+    if len(a) >= len(a[0]):
+        pass
+        
+
+    # 열이 행 보다 큰경우
+    else:
+        pass
+    
+    a = []
+
