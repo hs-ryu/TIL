05a1e88dad5851b3c3e71215b62660a29230ef6d
ryu <munam4@naver.com>
2022-09-12 22:23:40 +0900

    220912 백준 1문제 - 1018_체스판다시칠하기

--- python/알고리즘/baek/브루트포스/1018_체스판다시칠하기.py
+++ python/알고리즘/baek/브루트포스/1018_체스판다시칠하기.py
@@ -14,8 +14,37 @@
 '''
 
 #새삼 못풀겠다 포기 저녁에 다시생각해보기
-N, M = map(int, input().split())
+N,M = map(int, input().split())
+board = []
+for _ in range(N) :
+    board.append(input())
 
-for i in range(N-7):
-    for j in range(M-7):
-        
+w_case = "WB"*4 + "BW"*4
+b_case = "BW"*4 + "WB"*4
+min = 2174000
+cnt_1 = 0
+cnt_2 = 0
+
+for r in range(N-7):
+    for c in range(M-7):
+
+        cnt_1 = 0
+        cnt_2 = 0
+        sector = ""
+
+        for row in board[r:r+8] :
+            sector += row[c:c+8]
+            if len(sector) == 16:
+                for i in range(16) :
+                    if sector[i] != w_case[i]:
+                        cnt_1 += 1
+                    elif sector[i] != b_case[i] :
+                        cnt_2 += 1
+                sector = ""
+
+        if cnt_1 <= cnt_2 and cnt_1 < min :
+            min = cnt_1
+        elif cnt_2 < cnt_1 and cnt_2 < min :
+            min = cnt_2
+
+print(min)
