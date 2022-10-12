ae2453335090400aa246410cfd3a7cf399094b4b
ryu <munam4@naver.com>
2022-10-12 19:57:08 +0900

    221012 백준 1문제 - 14891_톱니바퀴

--- python/알고리즘/baek/구현/14891_톱니바퀴.py
+++ python/알고리즘/baek/구현/14891_톱니바퀴.py
@@ -1,42 +1,66 @@
-from collections import deque
-t = [deque(list(map(int,input().rstrip()))) for _ in range(4)] # 톱니 상태 저장
+# sawtooths = [list(input()) for _ in range(4)]
+# print(sawtooths)
+# k = int(input())
 
-k = int(input())
-for _ in range(k): 
-	r = []
-	for i in range(4):
-		r.append([t[i][6],t[i][2]])
-	n,d = map(int,input().split())
-	n = n-1
-    
-	if n != 0 :
-		for i in range(n,0,-1):
-			if r[i][0] != r[i-1][1]:
-				if (n-(i-1))%2 == 0:
-					t[i-1].rotate(d)
-				elif (n-(i-1))%2 != 0:
-					t[i-1].rotate(-1*d)
-			elif r[i][0] == r[i-1][1]:
-				break
+# for _ in range(k):
+# 	idx, dir = map(int,input().split())
+# 	idx -= 1
+# 	# idx에 따라 회전시킬 놈 찾기.
+# 	t1 = idx - 1
+# 	t2 = idx + 1
 
-	if n != 3:
-		for i in range(n,3):
-			if r[i][1] != r[i+1][0]:
-				if (n-(i+1))%2 == 0:
-					t[i+1].rotate(d)
-				elif (n-(i+1))%2 != 0:
-					t[i+1].rotate(-1*d)
-			elif r[i][1] == r[i+1][0]:
-				break
-	t[n].rotate(d)
+# 	# 왼쪽놈들 체크
+# 	while t1 >= 0:
 
-res = 0
-if t[0][0] == 1:
-	res+=1
-if t[1][0] == 1:
-	res+=2
-if t[2][0] == 1:
-	res+=4
-if t[3][0] ==1:
-	res+=8
-print(res)
+# 		t1 -= 1
+	
+
+# 	# 오른쪽 놈들 체크
+# 	while t2 < 4:
+
+# 		t2 += 1
+		
+# 	# 돌려야 할 놈들 돌리기.
+
+def rotate(i, dir):
+    #시계방향
+    if dir == 1 :
+        arr[i].insert(0,arr[i].pop())
+    #반시계방향
+    elif dir == -1:
+        arr[i].append(arr[i].pop(0))
+
+arr = []
+for _ in range(4):
+    arr.append(list(input()))
+K = int(input())
+
+for _ in range(K):
+    i, dir = map(int,input().split())
+    rotate_arr = [[i-1,dir]]
+
+    #오른쪽
+    x = i-1
+    tempdir = dir
+    while x + 1 <= 3:
+        if arr[x][2] != arr[x+1][6] :
+            tempdir = -tempdir
+            rotate_arr.append([x+1, tempdir])
+        elif arr[x][2] == arr[x+1][6]:
+            break
+        x += 1
+
+    #왼
+    x = i-1
+    tempdir = dir
+    while x - 1 >= 0 :
+        if arr[x][6] != arr[x-1][2]:
+            tempdir = -tempdir
+            rotate_arr.append([x -1, tempdir])
+        elif arr[x][6] == arr[x-1][2]:
+            break
+        x -= 1
+    for x, dd in rotate_arr:
+        rotate(x,dd)
+
+print(arr[0][0] * 1 + arr[1][0] * 2 + arr[2][0] * 4 + arr[3][0] * 8)
