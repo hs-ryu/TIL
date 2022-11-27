abe5e8f4cf0471f48a76ab882f67ee01c6c6e28a
ryu <munam4@naver.com>
2022-11-27 18:57:06 +0900

    221127 백준 1문제 - 21317_징검다리건너기(failed)

--- python/알고리즘/baek/DP/21317_징검다리건너기.py
+++ python/알고리즘/baek/DP/21317_징검다리건너기.py
@@ -1,20 +1,21 @@
+# 1퍼에서 틀림.
 n = int(input())
-if n == 1:
-    print(0)
-else: 
-    jumps = []
-    for i in range(n-1):
-        jumps.append(list(map(int,input().split())))
-    k = int(input())
 
-    dp = [5000 * 21] * n
-    dp[0] = 0
-    for i in range(n-1):
-        one, two = jumps[i]
-        if i + 1 < n:
-            dp[i+1] = min(dp[i] + one, dp[i+1])
-        if i + 2 < n:
-            dp[i+2] = min(dp[i] + two, dp[i+2])
-        if i + 3 < n:
-            dp[i+3] = min(dp[i] + k, dp[i+3])
-    print(dp[n-1])
+jumps = []
+for i in range(n-1):
+    jumps.append(list(map(int,input().split())))
+k = int(input())
+
+# dp 돌리면서, 입력 받은 작은점프, 큰점프, 매우 큰점프로 최소값 구해주기.
+dp = [5000 * 21] * n
+dp[0] = 0
+for i in range(n-1):
+    one, two = jumps[i]
+    if i + 1 < n:
+        dp[i+1] = min(dp[i] + one, dp[i+1])
+    if i + 2 < n:
+        dp[i+2] = min(dp[i] + two, dp[i+2])
+    if i + 3 < n:
+        dp[i+3] = min(dp[i] + k, dp[i+3])
+# 젤 끝에놈 출력. 근데 왜 틀리지.
+print(dp[n-1])
