a7e825d5548413b4950bf78c65607f7d36687324
ryu <munam4@naver.com>
2023-02-19 15:08:51 +0900

    230219 백준 1문제 - 1197_최소스패닝트리(python)

---
+++ python/알고리즘/baek/그래프탐색/1197_최소스패닝트리.py
@@ -0,0 +1,26 @@
+def find(x):
+    if x != graph[x]:
+        graph[x] = find(graph[x])
+    return graph[x]
+
+v,e = map(int,input().split())
+
+graph = [0] + [i+1 for i in range(v)]
+info = []
+
+for i in range(e):
+    info.append(list(map(int,input().split())))
+    
+info.sort(key=lambda x:x[2])
+
+result = 0
+for s,e,c in info:
+    a = find(s)
+    b = find(e)
+    if a != b:
+        if a > b:
+            graph[a] = b
+        else:
+            graph[b] = a
+        result += c
+print(result)
