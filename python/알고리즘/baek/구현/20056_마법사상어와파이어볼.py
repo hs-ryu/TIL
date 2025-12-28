880db0cb71d06733e5b44e7fb536b496205ace64
ryu <munam4@naver.com>
2025-12-28 18:35:51 +0900

    251228

--- python/알고리즘/baek/구현/20056_마법사상어와파이어볼.py
+++ python/알고리즘/baek/구현/20056_마법사상어와파이어볼.py
@@ -1,35 +1,46 @@
-
-# 상, 우상, 우, 우하, 하, 좌하, 좌, 좌상
-dr = [-1,-1,0,1,1,1,0,-1]
-dc = [0,1,1,1,0,-1,-1,-1]
-
-def move(cr, cc, info, new_map):
-    mass, speed, direction = info
-    new_r = (cr + speed * dr[direction]) % n
-    new_c = (cc + speed * dc[direction]) % n
-    if new_map[new_r][new_c] == 0:
-        new_map[new_r][new_c] = [mass, speed, direction]
-    else:
-        new_map[new_r][new_c].append([mass, speed, direction])
+dr = [-1, -1, 0, 1, 1, 1, 0, -1]
+dc = [0, 1, 1, 1, 0, -1, -1, -1]  
 
 
-n,m,k = list(map(int, input().split()))
+N, M, K = map(int, input().split())  
+arr = [[[] for _ in range(N)] for _ in range(N)]  
+fireball = []  
 
-arr = [[0] * n for i in range(n)]
 
 
-for i in range(m):
-    a,b,c,d,e = map(int, input().split())
-    arr[a-1][b-1] = [c,d,e]
+for _ in range(M):  
+    r, c, m, s, d = map(int, input().split())  
+    fireball.append((r-1, c-1, m,s,d))  
 
-cnt = 0
-while cnt < k:
-    cnt += 1
-    new_map = [[0] * n for i in range(n)]
+for _ in range(K):  
+    while fireball:  
+        cr, cc, cm, cs, cd = fireball.pop()  
+        nr = (cr + cs * dr[cd]) % N  
+        nc = (cc + cs * dc[cd]) % N  
+        arr[nr][nc].append([cm, cs, cd])  
 
-    for i in range(n):
-        for j in range(n):
-            if arr[i][j] != 0:
-                move(i, j, arr[i][j], new_map)
+    for r in range(N):  
+        for c in range(N):  
+            if len(arr[r][c]) >= 2:  
+                val_m, val_s, cnt_odd, cnt_even, cnt = 0, 0, 0, 0, len(arr[r][c])  
+                while arr[r][c]:  
+                    m, s, d = arr[r][c].pop()  
+                    val_m += m  
+                    val_s += s  
+                    if d % 2:  
+                        cnt_odd += 1  
+                    else:  
+                        cnt_even += 1  
+                if cnt_odd == cnt or cnt_even == cnt:  
+                    nd = [0, 2, 4, 6]  
+                else:  
+                    nd = [1, 3, 5, 7]  
 
-    arr = new_map[:]
+                if val_m // 5:  
+                    for i in nd:  
+                        fireball.append([r, c, val_m//5, val_s//cnt, i])  
+
+            elif len(arr[r][c]) == 1:  
+                fireball.append([r, c] + arr[r][c].pop())  
+
+print(sum(i[2] for i in fireball))
